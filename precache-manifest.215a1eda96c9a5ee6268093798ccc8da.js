c7d90132267d6f9a37c9b9182a144fb290d69cde
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2020-07-16 02:02:02 -0400

    Updates

---
+++ precache-manifest.215a1eda96c9a5ee6268093798ccc8da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3440f37ce87d7fc3c3caeec8b3cac2b5",
+    "url": "/courseweb/index.html"
+  },
+  {
+    "revision": "abfd4d915aac272803b6",
+    "url": "/courseweb/static/js/2.7cdb63d3.chunk.js"
+  },
+  {
+    "revision": "9ba7a5bf63c7ca8cb3b3d724251f5b20",
+    "url": "/courseweb/static/js/2.7cdb63d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fabc71ed12121c4a4f",
+    "url": "/courseweb/static/js/main.da763321.chunk.js"
+  },
+  {
+    "revision": "2127bcc43ced2e05e4f8",
+    "url": "/courseweb/static/js/runtime-main.5a506cf7.js"
+  }
+]);
